8e8d899c6e244f97f1a6cbcae461b76065cdef08
DESKTOP-PFMOHCA\jonny <jonny1996_ty@yahoo.co.jp>
2022-05-21 15:16:06 +0900

    とりあえずカスタムサーバーでsocketいけた

---
+++ server.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import * as http from "http";
+import next from "next";
+import * as socketio from "socket.io";
+
+const port = parseInt(process.env.PORT || "3000", 10);
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const nextHandler = nextApp.getRequestHandler();
+
+nextApp.prepare().then(async () => {
+  const app = express();
+  const server = http.createServer(app);
+  const io = new socketio.Server();
+  io.attach(server);
+
+  io.on("connection", (socket: socketio.Socket) => {
+    console.log("connection");
+    socket.emit("hello", "Hello from Socket.io");
+
+    socket.on("disconnect", () => {
+      console.log("client disconnected");
+    });
+    socket.on("emit", (data) => {
+      console.log(data);
+    });
+  });
+
+  app.all("*", (req: any, res: any) => nextHandler(req, res));
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
